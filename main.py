0ef5aa0f8f6d937368e6187348f42e30b1644973
cache-monet <remiaston@gmail.com>
2019-04-23 10:38:49 -0400

    added support for multi-user collection

--- main.py
+++ main.py
@@ -1,57 +1,39 @@
 import time
 from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
 from bs4 import BeautifulSoup
+from typing import List, Dict
 
 # Map of subreddits and number of posts
 
-def user_analytics(user_name: str, limit: int = 100):
+def collector(users: List[str], limit: int = 100) -> Dict[str, dict]:
     """
     Collects analytics on an user' recent acvitvity
+
+    :param users: list of usernames
+    :param limit: upper limit on how many posts crawler collects defaults to 100 posts
     """
-    usr = "https://reddit.com/user/{}".format(user_name)
+    post_analytics = {}
+    for u in users:
+        post_analytics[u] = parse_posts("https://reddit.com/user/{}/posts".format(u), limit)
 
-    # post analytics
-    posts = parse_posts(usr + "/posts/", limit)
-    total_posts = 0
-    for r, p in posts.items():
-        print("{} posts in in {}".format(p, r))
-        total_posts += p
-    print(sum)
-
-    # comments analytics
-    # comments = parse_comments(usr + "/comments/", limit)
-    # total_comments = 0
-    # for r, c in comments.items():
-    #     print("{} comments in in {}".format(c, r))
+    for user, posts in post_analytics.items():
+        print("User {} has:".format(user))
+        total_posts = 0
+        for r, p in posts.items():
+            print("{} posts in in {}".format(p, r))
+            total_posts += p
+        print("{} total posts\n".format(total_posts))
     
-def parse_comments(url: str, limit) -> dict:
-    """
-    Collects user comment history
-    """
-
 def parse_posts(url: str, limit: int) -> dict:
     """
     Collects user post history
+
+    :param url: url to user posts
+    :param limit: upper bound on how many posts to collect
     """
-    scroll = "window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight;return lenOfPage;"
-
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    browser = webdriver.Chrome(chrome_options=options)
-    browser.get(url)
-    lenOfPage, posts = 0, 0
-    match = False
-    while(not match and posts < limit):
-        lastCount = lenOfPage
-        time.sleep(0.5)
-        lenOfPage = browser.execute_script(scroll)
-        match, posts = lastCount == lenOfPage, posts+15
-
-    parsed = BeautifulSoup(browser.page_source, 'html.parser')
+    posts_per_page = 17 
+    page_html = process_page(url, limit//posts_per_page + 1)
+    parsed = BeautifulSoup(page_html, 'html.parser')
     rs, c = {}, 0
     for _, link in enumerate(parsed.select('a')):
         if link.has_attr('data-click-id') and link['data-click-id'] == 'subreddit':
@@ -60,4 +42,22 @@ def parse_posts(url: str, limit: int) -> dict:
             if c >= limit: return rs
     return rs
     
-user_analytics('skiing2022')
+def process_page(url: str, scrolls: int):
+    """
+    Process the page and returns source code
+
+    :param url: page url
+    :param scrolls: number of time to scroll to bottom (for infinite scroll pages) 
+    """
+    scroll_script = "window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight;return lenOfPage;"
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.get(url)
+    lenOfPage = 0
+    match = False
+    while(not match and scrolls > 0):
+        lastCount = lenOfPage
+        lenOfPage = browser.execute_script(scroll_script)
+        match, srolls = lastCount == lenOfPage, scrolls-1
+    return browser.page_source
